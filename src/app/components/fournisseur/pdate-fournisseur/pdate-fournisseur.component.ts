cf307793e48c6fbda6658648cfc5212e57668ffa
ABDESSADEQMAKKIOUI <abdessadeq.elmakkioui20@gmail.com>
2024-01-28 14:35:45 +0100

    realisation du fonctionaluté de modifier les information d'un fournisseur

--- src/app/components/fournisseur/pdate-fournisseur/pdate-fournisseur.component.ts
+++ src/app/components/fournisseur/pdate-fournisseur/pdate-fournisseur.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Fournisseur} from "../../../entities/fournisseur/fournisseur";
+import {FournisseurService} from "../../../services/Fournisseur/fournisseur.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import * as console from "console";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-pdate-fournisseur',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PdateFournisseurComponent implements OnInit {
 
-  constructor() { }
+  public updatedfournisseur!: Fournisseur;
+  constructor( private fournisseurservic :FournisseurService) { }
 
   ngOnInit(): void {
   }
-
+  public UpdateFournisseur(fournisseur: Fournisseur): void {
+    this.fournisseurservic.updateFournisseur(fournisseur).subscribe(
+      (response: Fournisseur) => {
+        console.log(response);
+        this.fournisseurservic.getFournisseurs();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
