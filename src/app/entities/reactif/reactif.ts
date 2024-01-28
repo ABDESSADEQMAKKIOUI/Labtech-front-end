a79aff881644ca0a9be2df22a2c1a62b915999b4
ABDESSADEQMAKKIOUI <abdessadeq.elmakkioui20@gmail.com>
2024-01-28 11:59:07 +0100

    realisation d'entity reactif

--- src/app/entities/reactif/reactif.ts
+++ src/app/entities/reactif/reactif.ts
@@ -1,2 +1,30 @@
+import {Fournisseur} from "../fournisseur/fournisseur";
+
 export class Reactif {
+  idReactif?: number;
+  nom: string;
+  description: string;
+  quantite: number;
+  dateExpiration: Date;
+  fournisseur: Fournisseur;
+  deleted: boolean;
+
+  constructor(
+    nom: string,
+    description: string,
+    quantite: number,
+    dateExpiration: Date,
+    fournisseur: Fournisseur,
+    deleted: boolean
+  ) {
+    this.nom = nom;
+    this.description = description;
+    this.quantite = quantite;
+    this.dateExpiration = dateExpiration;
+    this.fournisseur = fournisseur;
+    this.deleted = deleted;
+  }
 }
+
+
+
