db724130a2a116b7672be60bba7adf460eb5ff8d
adilfettach <adilerrai24@gmail.com>
2024-01-29 21:41:28 +0100

    creating Utilisateur service

--- src/app/components/utilisateur/utilisateurs/utilisateurs.component.ts
+++ src/app/components/utilisateur/utilisateurs/utilisateurs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UtilisateurService} from "../../../services/utilisateur/utilisateur.service";
+import {Utilisateur} from "../../../entities/utilisateur/utilisateur";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-utilisateurs',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UtilisateursComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private utilisateurService: UtilisateurService) { }
+  utilisateurs: Utilisateur[] = [];
   ngOnInit(): void {
+    this.getUtilisateurs();
   }
-
+  getUtilisateurs(): void {
+    this.utilisateurService.getAllUsers().subscribe(
+      (response: Utilisateur[]) => {
+        console.log('API Response:', response);
+        this.utilisateurs = response;
+        console.log('Updated utilisateurs:', this.utilisateurs);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
